016cbffe096cd3fbfaeea3d6247452c2daf8e916
chaithanyahs13 <chaithanyahs13@gmail.com>
2024-01-18 19:27:40 +0530

    deleted unwanted letter

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function determineWinner(choice1, choice2) {
     (choice1 === "rock" && choice2 === "scissors") ||
     (choice1 === "paper" && choice2 === "rock") ||
     (choice1 === "scissors" && choice2 === "paper")
-  ) {s
+  ) {
     return "You win!";
   } else {
     return "Computer wins!";
